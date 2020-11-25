21f539baefd1ab8bdd6c04a779f680f677c852c3
guokangkang <guokangkang>
2020-11-25 23:50:23 +0800

    vue

--- src/main/java/com/borrow/book/controller/TestController.java
+++ src/main/java/com/borrow/book/controller/TestController.java
@@ -2,19 +2,17 @@ package com.borrow.book.controller;
 
 
 import com.borrow.book.domain.ResultBody;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 
-@RestController
+@Controller
 public class TestController {
     @GetMapping("/hello")
-    public Flux<ResultBody> hello() {   //
-        ResultBody resultBody=new ResultBody();
-        resultBody.setCode("200");
-        resultBody.setMessage("返回成功");
-        return null;
+    public String hello() {   //
+        return "index";
     }
 
 
