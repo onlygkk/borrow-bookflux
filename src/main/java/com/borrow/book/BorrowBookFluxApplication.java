4e91482d9a472679f7e32bb964cad5ebe4f4baab
15962958591@163.com <15962958591@163.com>
2020-11-26 16:26:48 +0800

    mybatis配置

--- src/main/java/com/borrow/book/BorrowBookFluxApplication.java
+++ src/main/java/com/borrow/book/BorrowBookFluxApplication.java
@@ -1,9 +1,12 @@
 package com.borrow.book;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.borrow.book.mapper")
 public class BorrowBookFluxApplication {
     public static void main(String[] args) {
         SpringApplication.run(BorrowBookFluxApplication.class, args);
---
+++ src/main/java/com/borrow/book/entity/User.java
@@ -0,0 +1,4 @@
+package com.borrow.book.entity;
+
+public class User {
+}
