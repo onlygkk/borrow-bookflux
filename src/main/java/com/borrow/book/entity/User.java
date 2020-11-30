f5859d5497bf1203f07e133b9bbd38edd5dd6d85
15962958591@163.com <15962958591@163.com>
2020-11-30 14:15:36 +0800

    Lambda_Test2

--- src/main/java/com/borrow/book/entity/User.java
+++ src/main/java/com/borrow/book/entity/User.java
@@ -4,6 +4,14 @@ import lombok.Data;
 
 @Data
 public class User {
+
+    public User(int id,String userName,String passWord,String realName){
+        this.id=id;
+        this.userName=userName;
+        this.passWord=passWord;
+        this.realName=realName;
+
+    }
     private int id;
 
     private String userName;
--- src/test/java/com/borrow/book/controller/EsApiApplicationTest.java
+++ src/test/java/com/borrow/book/controller/EsApiApplicationTest.java
@@ -1,6 +1,7 @@
 package com.borrow.book.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.borrow.book.entity.User;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -24,7 +25,7 @@ public class EsApiApplicationTest {
     @Autowired
     private com.borrow.book.mapper.UserMapper userMapper;
     @Test
-    public void TestCreateIndex() throws Exception {
+    public void testCreateIndex() throws Exception {
 
           /*IndexRequest request =new IndexRequest("idx");
             request.id("1");
@@ -47,16 +48,15 @@ public class EsApiApplicationTest {
     }
 
     @Test
-    public void SqlTest() {
+    public void sqlTest() {
         //System.out.println(JSON.toJSONString(userMapper.QueryUserList()));
-
         List<String> languages = Arrays.asList("java","scala","python");
         languages.forEach(language ->{
             System.out.println(language);
         });
     }
     @Test
-    public void LamdbaTest()
+    public void lamdbaTest()
     {
        List<Double> cost = Arrays.asList(10.0, 20.0,30.0);
        Double aDouble = cost.stream().map(x -> x + x * 1).reduce((sum, x) -> sum + x).get();
@@ -86,8 +86,35 @@ public class EsApiApplicationTest {
         list.add("D");
         list.add("E");
         //输出：A,B,C,D,E
-        list.forEach(item->System.out.println(item));
+        list.forEach(item->System.out.println("Stream:"+item));
 
+
+        list.parallelStream().forEach(num ->System.out.println(Thread.currentThread().getName()+">>"+num));
+        Runnable runnable =() ->{
+            System.out.println("Thread");
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+
+
+        String name[] = {"张三", "李四", "王五", "孙刘", "赵强", "李明", "赵强", "汪汪"};
+        System.out.println(Arrays.stream(name).filter(x -> x.equals("赵强")).count());
+        List<User> userList =Arrays.asList(new User(1,"zhansan" , "123","张三") , new User(1,"lisi" , "123","李四") , new User(3,"wangwu", "123","王五"));
+        System.out.println(userList.stream().map(User::getRealName).collect(Collectors.joining(",","{","}")));
+        //取值
+        List<String> collect = userList.stream().map(User::getUserName).collect(Collectors.toList());
+
+        System.out.println("取值："+JSON.toJSONString(collect));
+        //分组
+        Map<Integer, List<User>> groupByCollect = userList.stream().collect(Collectors.groupingBy(User::getId));
+        System.out.println(JSON.toJSONString(groupByCollect));
+
+
+        List<String> stringList2 = Arrays.stream(name).filter(x -> !x.equals("赵强")).map(y -> y + "加后缀").collect(Collectors.toList());
+        System.out.println(stringList2);
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        int reduce = Arrays.stream(a).reduce(0, (b, c) -> b + c);
 
     }
 }
