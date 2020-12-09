b169737ef1f9bf7297e048915c125fca1d0439d2
guokangkang <guokangkang>
2020-12-09 21:53:32 +0800

    canel修改

--- src/main/java/com/borrow/book/controller/TestController.java
+++ src/main/java/com/borrow/book/controller/TestController.java
@@ -3,11 +3,11 @@ package com.borrow.book.controller;
 
 import com.borrow.book.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class TestController {
     @Autowired
     private UserMapper userMapper;
