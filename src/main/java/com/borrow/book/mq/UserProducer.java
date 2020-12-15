2e33838fa2003fde0fe7adeec7529bf96342d7dc
15962958591@163.com <15962958591@163.com>
2020-12-15 14:37:07 +0800

    1

---
+++ src/main/java/com/borrow/book/mq/UserProducer.java
@@ -0,0 +1,46 @@
+package com.borrow.book.mq;
+
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class UserProducer {
+
+    /**
+     * 生产者的组名
+     */
+    private String producerGroup="user-group";
+    /**
+     * NameServer 地址
+     */
+    private String namesrvAddr="127.0.0.1:9876";
+
+    @PostConstruct
+    public void produder() {
+        DefaultMQProducer producer = new DefaultMQProducer(producerGroup);
+        producer.setNamesrvAddr(namesrvAddr);
+        try {
+            producer.start();
+            for (int i = 0; i < 5; i++) {
+               String jsonstr= "{\"2\":\"mqtest-keys\"}";
+                System.out.println("发送消息:"+jsonstr);
+                Message message = new Message("user-topic", "user-tag","test-key",jsonstr.getBytes(RemotingHelper.DEFAULT_CHARSET));
+                SendResult result = producer.send(message);
+                System.err.println("发送响应：MsgId:" + result.getMsgId() + "，发送状态:" + result.getSendStatus());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.shutdown();
+        }
+    }
+
+}
